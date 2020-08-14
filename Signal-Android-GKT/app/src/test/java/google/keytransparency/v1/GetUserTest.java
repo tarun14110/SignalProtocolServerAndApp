b8f2b1f561c43d49ccad339aa1814698d9de02bf
Ryan West <rwest@qualtrics.com>
2020-08-13 20:42:39 -0600

    add file to test key transparency
    Signed-off-by: Ryan West <rwest@qualtrics.com>

---
+++ Signal-Android-GKT/app/src/test/java/google/keytransparency/v1/GetUserTest.java
@@ -0,0 +1,13 @@
+package google.keytransparency.v1;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+public class GetUserTest {
+    @Test
+    public void getUser() {
+//        make a KeyTransparencyGrpc.KeyTransparencyBlockingStub()
+//        call it
+//        assertEquals(0, response);
+    }
+}
