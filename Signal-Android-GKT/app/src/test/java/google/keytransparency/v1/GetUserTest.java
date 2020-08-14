da4399ede8296316f0b8e91d1841e11781b08c71
Ryan West <rwest@qualtrics.com>
2020-08-13 22:01:01 -0600

    progress on getusertest
    Signed-off-by: Ryan West <rwest@qualtrics.com>

--- Signal-Android-GKT/app/src/test/java/google/keytransparency/v1/GetUserTest.java
+++ Signal-Android-GKT/app/src/test/java/google/keytransparency/v1/GetUserTest.java
@@ -1,13 +1,26 @@
 package google.keytransparency.v1;
 
 import org.junit.Test;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 import static junit.framework.Assert.assertEquals;
 
 public class GetUserTest {
+    String target = "johnbrooke.cs.byu.edu:8080";
+    ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+
     @Test
     public void getUser() {
-//        make a KeyTransparencyGrpc.KeyTransparencyBlockingStub()
-//        call it
-//        assertEquals(0, response);
+        // todo get right port
+//      ManagedChannelBuilder builder = ManagedChannelBuilder.forAddress("johnbrooke.cs.byu.edu", 8080)
+        KeyTransparencyGrpc.KeyTransparencyBlockingStub  blockingStub = KeyTransparencyGrpc.newBlockingStub(channel);
+
+        // TODO: first thing is figure out how to make a GetUserRequest. It has a private constructor
+        // so you have to construct it another way, not sure
+//      Gkt.GetUserRequest req = new Gkt.GetUserRequest()
+//      Gkt.GetUserResponse resp = blockingStub.getUser(req)
+//      assertEquals(whatever we think response should be, resp);
     }
 }
